2b81035b670732b0ed4edd1b65f4ec82a1135d64
chaldrak <chaldrakus@gmail.com>
2021-10-01 14:52:21 +0100

    everyleaf step 2

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,14 +3,16 @@ RSpec.describe 'Fonction de gestion des tâches', type: :system do
   before do
     # あらかじめタスク一覧のtestで使用するためのタスクを二つ作成する
     FactoryBot.create(:task)
-    visit tasks_path
+    FactoryBot.create(:second_task)
   end
  
   describe "Nouvelle fonction de création" do
     context "Lors de la création d'une nouvelle tâche" do
       it "La tâche créée s'affiche" do
         task = FactoryBot.create(:task, name: 'task')
+        visit tasks_path
         expect(page).to have_content 'task'
+        expect(page).to have_content 'test_content'
       end
     end
   end
@@ -18,10 +20,12 @@ RSpec.describe 'Fonction de gestion des tâches', type: :system do
     context "Lors de la transition vers l'écran de liste" do
       it "La liste des tâches créées s'affiche" do
       end
+    end
     context "Lorsque les tâches sont organisées par ordre décroissant de date et d'heure de création" do
       it "La nouvelle tâche s'affiche en haut" do
+        visit tasks_path
         task_list = all('.task_row')
-        
+        expect(task_list[0]).to have_content 'test_name_second'
       end
     end
   end
