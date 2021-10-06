cb5cda409bdd56aff6f3ef32036a2b8e677fdfac
chaldrak <chaldrakus@gmail.com>
2021-10-06 08:09:00 +0100

    step4 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
     has_many :tasks, dependent: :destroy
+    before_destroy :check_if_is_last_admin, prepend: true
+    before_update :check_if_admin, prepend: true
+
+    private
+    def check_if_is_last_admin
+        throw :abort if User.where(is_admin: true).count == 1
+    end
+
+    def check_if_admin
+        self.is_admin = true if User.where(is_admin: true).count == 1
+    end
 end
